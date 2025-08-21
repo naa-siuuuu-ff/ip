198ef293c5539cddde7f7ff77d83385df3a537e2
naa-siuuuu-ff <nasifhasan2005@gmail.com>
2025-08-22 00:42:10 +0800

    Chatbot now echos properly

--- src/main/java/Ozil.java
+++ src/main/java/Ozil.java
@@ -1,10 +1,39 @@
+import java.util.Scanner;
+
 public class Ozil {
+    private static String intro = "____________________________________________________________\n"
+                + "Hello! I'm Ozil!\n"
+                + "What can I do for you?\n"
+                + "____________________________________________________________\n";
+
+    private static String bye = "____________________________________________________________\n"
+                + "Bye. Hope to see you again soon!\n"
+                + "____________________________________________________________\n";
+
+    private static String echo(String userinput) {
+        return "____________________________________________________________\n"
+                + userinput + "\n" +
+                "____________________________________________________________\n";
+    }
+
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________");
-        System.out.println("Hello! I'm Ozil!");
-        System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________\n");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println(intro);
+
+        while (true) {
+            String input = scanner.nextLine();
+
+            if (input.equalsIgnoreCase("bye")) {
+                break;
+            }
+
+            System.out.println(echo(input));
+        }
+
+        System.out.println(bye);
+        scanner.close();
+
+
     }
 }
